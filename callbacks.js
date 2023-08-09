89cf40fecde34f4a6297f46764cdf7163a4cb848
Tarun Kumar <tarunkumar2407@gmail.com>
2023-08-09 23:23:23 +0530

    initial commit

--- callbacks.js
+++ callbacks.js
@@ -19,12 +19,39 @@ app.get("/", (req, res) => {
 // app.get('/ab*cd', (req, res) => {
 //   res.send('ab*cd')
 // })
-app.get('/ab(cd)?e', (req, res) => {
-  res.send('ab(cd)?e')
-})
+// app.get('/ab(cd)?e', (req, res) => {
+//   res.send('ab(cd)?e')
+// })
 
-app.get('/ab(cd)?e', (req, res) => {
-  res.send('ab(cd)?e')
+// app.get('/ab(cd)?e', (req, res) => {
+//   res.send('ab(cd)?e')
+// })
+
+// callbacks
+// app.get('/callbacks', (req, res, next) => {
+//   console.log('the response will be sent by the next function ...')
+//   next()
+// }, (req, res) => {
+//   res.send('Hello from B!')
+// })
+
+// 
+
+const cb0 = function (req, res, next) {
+  console.log('CB0')
+  next()
+}
+
+const cb1 = function (req, res, next) {
+  console.log('CB1')
+  next()
+}
+
+app.get('/callbacks', [cb0, cb1], (req, res, next) => {
+  console.log('the response will be sent by the next function ...')
+  next()
+}, (req, res) => {
+  res.send('Hello from D!')
 })
 
 app.listen(3000, () => {
