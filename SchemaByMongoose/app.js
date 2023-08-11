9ba3847c6581e96143025cd26ceca24d64149ac8
Tarun Kumar <tarunkumar2407@gmail.com>
2023-08-12 00:03:32 +0530

    initial commit

--- SchemaByMongoose/app.js
+++ SchemaByMongoose/app.js
@@ -1,11 +1,14 @@
 import express from "express"
 import connectDB from "./db/connectDB.js";
-import "./models/Student.js"
+import createDoc from "./models/Student.js";
 const app = express();
 const DATABASE_URL = "mongodb://127.0.0.1:27017"
 
-console.log("Database created")
+//database connections
 connectDB(DATABASE_URL);
+
+//create and sove document
+createDoc("Soumya", 24, 5900.0, ["singing","dancing","teaching"], true, [{ value: "Mongoose is good" }],);
 
 app.listen('3000', () => {
     console.log("server is running at port 3000")
--- SchemaByMongoose/models/Student.js
+++ SchemaByMongoose/models/Student.js
@@ -1,17 +1,46 @@
 import mongoose from "mongoose";
 
-// creating schema
+// defining schema
 const studentSchema = new mongoose.Schema({
-    name:{type: String, required: true, trim: true},
-    age: {type: Number, required: true, min: 18, max: 50},
-    fees: {type: mongoose.Decimal128, validate: (v)=> v>=5500.50},
-    hobbies: {type: Array},
-    isActive: {type: Boolean},
-    comments: [{value: {type: String}, publish: {type: Date, default: Date.now}}],
-    join: {type: Date, default: Date.now}
-})
+  name: { type: String, required: true, trim: true },
+  age: { type: Number, required: true, min: 18, max: 50 },
+  fees: { type: mongoose.Decimal128, validate: (v) => v >= 5500.5 },
+  hobbies: { type: Array },
+  isActive: { type: Boolean },
+  comments: [
+    { value: { type: String }, publish: { type: Date, default: Date.now } },
+  ],
+  join: { type: Date, default: Date.now },
+});
 
 // console.log(studentSchema.path('age'))
 
 //compilling schema
-const studentModel = mongoose.model("student", studentSchema)
+const studentModel = mongoose.model("student", studentSchema);
+
+const createDoc = async (nm, ag, fe, hob, isAct, comm) => {
+  try {
+    //creating schema
+    // const studentDoc = new studentModel({
+    //   name: "Soumya",
+    //   age: 24,
+    //   fees: 5900.0,
+    //   hobbies: ["singing", "dancing", "teaching"],
+    //   isActive: true,
+    //   comments: [{ value: "Mongoose is good" }],
+    // });
+    const studentDoc = new studentModel({
+        name: nm,
+        age: ag,
+        fees: fe,
+        hobbies: hob,
+        isActive: isAct,
+        comments: comm,
+      });
+
+    //saving document
+    const result = await studentDoc.save();
+    console.log(result);
+  } catch (error) {console.log(error)}
+};
+export default createDoc
