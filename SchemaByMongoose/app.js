c7d38dc1beb22c0958578fa83fa1d7ed99c8ae65
Tarun Kumar <tarunkumar2407@gmail.com>
2023-08-12 00:19:27 +0530

    initial commit

--- SchemaByMongoose/app.js
+++ SchemaByMongoose/app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import connectDB from "./db/connectDB.js";
-import createDoc from "./models/Student.js";
+import { createMultipleDoc } from "./models/Student.js";
 const app = express();
 const DATABASE_URL = "mongodb://127.0.0.1:27017"
 
@@ -8,7 +8,9 @@ const DATABASE_URL = "mongodb://127.0.0.1:27017"
 connectDB(DATABASE_URL);
 
 //create and sove document
-createDoc("Soumya", 24, 5900.0, ["singing","dancing","teaching"], true, [{ value: "Mongoose is good" }],);
+// createDoc();
+createMultipleDoc();
+// createDoc("Soumya", 24, 5900.0, ["singing","dancing","teaching"], true, [{ value: "Mongoose is good" }],);
 
 app.listen('3000', () => {
     console.log("server is running at port 3000")
--- SchemaByMongoose/models/Student.js
+++ SchemaByMongoose/models/Student.js
@@ -18,29 +18,71 @@ const studentSchema = new mongoose.Schema({
 //compilling schema
 const studentModel = mongoose.model("student", studentSchema);
 
-const createDoc = async (nm, ag, fe, hob, isAct, comm) => {
-  try {
-    //creating schema
-    // const studentDoc = new studentModel({
-    //   name: "Soumya",
-    //   age: 24,
-    //   fees: 5900.0,
-    //   hobbies: ["singing", "dancing", "teaching"],
-    //   isActive: true,
-    //   comments: [{ value: "Mongoose is good" }],
-    // });
-    const studentDoc = new studentModel({
-        name: nm,
-        age: ag,
-        fees: fe,
-        hobbies: hob,
-        isActive: isAct,
-        comments: comm,
-      });
+// const createDoc = async (nm, ag, fe, hob, isAct, comm) => {
+//   try {
+//     //creating schema
+//     // const studentDoc = new studentModel({
+//     //   name: "Soumya",
+//     //   age: 24,
+//     //   fees: 5900.0,
+//     //   hobbies: ["singing", "dancing", "teaching"],
+//     //   isActive: true,
+//     //   comments: [{ value: "Mongoose is good" }],
+//     // });
+//     const studentDoc = new studentModel({
+//         name: nm,
+//         age: ag,
+//         fees: fe,
+//         hobbies: hob,
+//         isActive: isAct,
+//         comments: comm,
+//       });
 
-    //saving document
-    const result = await studentDoc.save();
-    console.log(result);
-  } catch (error) {console.log(error)}
-};
-export default createDoc
+//     //saving document
+//     const result = await studentDoc.save();
+//     console.log(result);
+//   } catch (error) {console.log(error)}
+// };
+
+const createMultipleDoc = async () => {
+    try {
+    //   creating schema
+      const rahulDoc = new studentModel({
+        name: "Rahul",
+        age: 21,
+        fees: 5900.0,
+        hobbies: ["singing", "dancing", "teaching"],
+        isActive: true,
+        comments: [{ value: "Mongoose is good" }],
+      });
+      const aditiDoc = new studentModel({
+        name: "Aditi",
+        age: 26,
+        fees: 5900.0,
+        hobbies: ["singing", "dancing", "teaching"],
+        isActive: true,
+        comments: [{ value: "Mongoose is good" }],
+      });
+      const priyaDoc = new studentModel({
+        name: "Priya",
+        age: 23,
+        fees: 5900.0,
+        hobbies: ["singing", "dancing", "teaching"],
+        isActive: true,
+        comments: [{ value: "Mongoose is good" }],
+      });
+      const subhiDoc = new studentModel({
+        name: "Shubhi",
+        age: 26,
+        fees: 5900.0,
+        hobbies: ["singing", "dancing", "teaching"],
+        isActive: true,
+        comments: [{ value: "Mongoose is good" }],
+      });
+      
+      //saving document
+      const result = await studentModel.insertMany([rahulDoc, priyaDoc, aditiDoc, subhiDoc]);
+      console.log(result);
+    } catch (error) {console.log(error)}
+  };
+export { createMultipleDoc }
